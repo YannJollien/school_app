caced11838a23a50aac498043d192529d3ad2be1
aurelienmay <48348880+aurelienmay@users.noreply.github.com>
2020-11-30 11:20:38 +0100

    Merge branch 'master' of C:\Users\aurel\flutterapps\school_app with conflicts.

--- lib/components/contactList.dart
+++ lib/components/contactList.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ContactList extends StatefulWidget {
-  String listName ;
+  String listName;
 
-  ContactList(String name){
+  ContactList(String name) {
     this.listName = name;
   }
 
@@ -23,39 +23,43 @@ class ContactListState extends State<ContactList> {
 
   Card buildItem(DocumentSnapshot doc) {
     return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              '${doc.data()['surname']}' + ' ' + '${doc.data()['name']}',
-              style: TextStyle(fontSize: 24),
-            ),
-            Text(
-              '${doc.data()['work']}',
-              style: TextStyle(fontSize: 20),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                SizedBox(width: 8),
-                FlatButton(
-                  onPressed: () => deleteData(doc),
-                  child: Text('Delete'),
-                ),
-                FlatButton(
-                  onPressed: () {
+      child: InkWell(
+        onTap: () {
+          //NAVIGATION PUSH
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    '${doc.data()['surname']}' + ' ' + '${doc.data()['name']}',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  SizedBox(width: 8),
+                  IconButton(
+                    icon: Icon(Icons.delete),
+                    color: Colors.blue,
+                    onPressed: () {},
+                  ),
+//                FlatButton(
+//                  onPressed: () {
 //                    Navigator.push(
 //                      context,
-//                      MaterialPageRoute(builder: (context) => Login()),
+//                      MaterialPageRoute(
+//                          builder: (context) =>
+//                              ContactList(doc.data()['listName'])),
 //                    );
-                  },
-                  child: Text('Show'),
-                ),
-              ],
-            )
-          ],
+//                  },
+//                  child: Text('Show'),
+//                ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
@@ -71,12 +75,20 @@ class ContactListState extends State<ContactList> {
         padding: EdgeInsets.all(8),
         children: <Widget>[
           StreamBuilder<QuerySnapshot>(
-            stream: db.collectionGroup(widget.listName.toLowerCase()).snapshots(),
+//            stream: db.collectionGroup(widget.listName.toLowerCase()).snapshots(),
+            stream: db
+                .collection('users')
+                .doc('134fJsKGo4fD2NEXhmVlPxUBTIh2')
+                .collection('lists')
+                .doc('MaSJtHSIGWl1UtVKCemx')
+                .collection(widget.listName.toLowerCase())
+                .snapshots(),
             builder: (context, snapshot) {
               if (snapshot.hasData) {
                 return Column(
-                    children: snapshot.data.docs.map((doc) => buildItem(doc)).toList()
-                );
+                    children: snapshot.data.docs
+                        .map((doc) => buildItem(doc))
+                        .toList());
               } else {
                 return SizedBox();
               }
@@ -84,10 +96,55 @@ class ContactListState extends State<ContactList> {
           )
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          createAlertDialog(context).then((value) => setState(() {
+            if (value.isNotEmpty) {
+              db
+                  .collection('users')
+                  .doc('134fJsKGo4fD2NEXhmVlPxUBTIh2')
+                  .collection('lists')
+                  .doc('MaSJtHSIGWl1UtVKCemx')
+                  .collection(widget.listName.toLowerCase())
+                  .add({
+                'name': value.substring(value.indexOf(' ')+1, value.length),
+                'surname': value.substring(0, value.indexOf(' ')),
+                'phone': '',
+                'email': '',
+                'institution': '',
+                'notes': ''
+              });
+            }
+          }));
+        },
+      ),
     );
   }
 
-  void deleteData(DocumentSnapshot doc) async {
-
+  Future<String> createAlertDialog(BuildContext context) {
+    TextEditingController customController = new TextEditingController();
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            content: TextField(
+              controller: customController,
+              decoration:
+              new InputDecoration(labelText: 'Full name', hintText: 'John Smith'),
+            ),
+            actions: <Widget>[
+              MaterialButton(
+                elevation: 5.0,
+                child: Text("Add"),
+                onPressed: () {
+                  Navigator.of(context).pop(customController.text.toString());
+                },
+              ),
+            ],
+          );
+        });
   }
+
+  void deleteData(DocumentSnapshot doc) async {}
 }
--- lib/components/lists.dart
+++ lib/components/lists.dart
@@ -22,38 +22,69 @@ class ListsState extends State<Lists> {
 
   Card buildItem(DocumentSnapshot doc) {
     return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              '${doc.data()['listName']}',
-              style: TextStyle(fontSize: 24),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                SizedBox(width: 8),
-                FlatButton(
-                  onPressed: () => _listService.deleteLists(doc),
-                  child: Text('Delete'),
-                ),
-                FlatButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => ContactList(doc.data()['listName'])),
-                    );
-                  },
-                  child: Text('Show'),
-                ),
-              ],
-            )
-          ],
+      child: InkWell(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => ContactList(doc.data()['listName'])),
+          );
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    '${doc.data()['listName']}',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  SizedBox(width: 8),
+                  IconButton(
+                    icon: Icon(Icons.delete),
+                    color: Colors.blue,
+                    onPressed: () {
+                      db
+                          .collection('users')
+                          .doc('134fJsKGo4fD2NEXhmVlPxUBTIh2')
+                          .collection('lists')
+                          .doc(doc.data()['listName'])
+                          .delete();
+                    },
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Future<String> createAlertDialog(BuildContext context) {
+    TextEditingController customController = new TextEditingController();
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Category"),
+            content: TextField(
+              controller: customController,
+            ),
+            actions: <Widget>[
+              MaterialButton(
+                elevation: 5.0,
+                child: Text("Add"),
+                onPressed: () {
+                  Navigator.of(context).pop(customController.text.toString());
+                },
+              ),
+            ],
+          );
+        });
   }
 
   @override
@@ -66,24 +97,70 @@ class ListsState extends State<Lists> {
         padding: EdgeInsets.all(8),
         children: <Widget>[
           StreamBuilder<QuerySnapshot>(
-            stream: _listService.getLists(),
-            builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+//            stream: db.collectionGroup('lists').snapshots(),
+            stream: db
+                .collection('users')
+                .doc('134fJsKGo4fD2NEXhmVlPxUBTIh2')
+                .collection('lists')
+                .snapshots(),
+            builder: (context, snapshot) {
               if (snapshot.hasData) {
                 return Column(
                   children:
-                  snapshot.data.docs.map((doc) => buildItem(doc)).toList(),
+                      snapshot.data.docs.map((doc) => buildItem(doc)).toList(),
                 );
               } else {
                 return SizedBox();
               }
             },
           )
+//
+//          FutureBuilder(
+//              future: _listService.getLists(),
+//              builder: (context, snapshot) {
+//                if (snapshot.hasData) {
+//                  return Column(
+//                    children: snapshot.data.docs
+//                        .map((doc) => buildItem(doc))
+//                        .toList(),
+//                  );
+//                } else {
+//                  return SizedBox();
+//                }
+//              })
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          createAlertDialog(context).then((value) => setState(() {
+                db
+                    .collection('users')
+                    .doc('134fJsKGo4fD2NEXhmVlPxUBTIh2')
+                    .collection('lists')
+                    .doc('1')
+                    .set({'listName': value});
+                db
+                    .collection('users')
+                    .doc('134fJsKGo4fD2NEXhmVlPxUBTIh2')
+                    .collection('lists')
+                    .doc('1')
+                    .collection(value)
+                    .add({
+                  'name': '',
+                  'surname': 'Auto creation',
+                  'phone': '',
+                  'email': '',
+                  'institution': '',
+                  'notes': ''
+                });
+//                    .set({'test': value});
+//                    .add({'listName': value});
+              }));
+        },
       ),
     );
   }
 
-  void deleteData(DocumentSnapshot doc) async {
-
-  }
+  void deleteData(DocumentSnapshot doc) async {}
 }
