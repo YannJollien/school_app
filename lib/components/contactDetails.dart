f4498aebbf7f7f301e7c674241209b675c7a65c2
aurelienmay <48348880+aurelienmay@users.noreply.github.com>
2020-12-03 11:41:16 +0100

    Contact details modifications

--- lib/components/contactDetails.dart
+++ lib/components/contactDetails.dart
@@ -165,7 +165,7 @@ class ContactDetailsState extends State<ContactDetails> {
         suffixIcon: IconButton(
           color: Colors.black,
           onPressed: () {
-
+              //Save the notes
           },
           icon: Icon(Icons.save),
         ),
