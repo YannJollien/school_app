5872d4a8d4dcc68b2ed5b47793cb513d46162fa8
aurelienmay <48348880+aurelienmay@users.noreply.github.com>
2021-01-11 16:06:35 +0100

    Contact importation with csv and initial avatar done.

--- lib/components/contact_add/contactListImportation.dart
+++ lib/components/contact_add/contactListImportation.dart
@@ -1,22 +1,29 @@
+import 'dart:async';
+import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_document_picker/flutter_document_picker.dart';
+import 'package:schoolapp/components/contact_list/contactsFromList.dart';
+import 'package:schoolapp/services/contactService.dart';
 
-class ListImportation extends StatefulWidget {
-  DocumentSnapshot listDoc;
+class ContactListImportation extends StatefulWidget {
+  static DocumentSnapshot listDoc;
 
-  ListImportation(DocumentSnapshot doc) {
-    this.listDoc = doc;
+  ContactListImportation(DocumentSnapshot doc) {
+    listDoc = doc;
   }
 
   @override
-  State<StatefulWidget> createState() => new ListImportationState(listDoc);
+  State<StatefulWidget> createState() => new ContactListImportationState(listDoc);
 }
 
-class ListImportationState extends State<ListImportation> {
-  ListImportationState(data);
+class ContactListImportationState extends State<ContactListImportation> {
+  ContactListImportationState(data);
 
-  String _filePath;
+  ContactService _contactService = ContactService();
 
   @override
   Widget build(BuildContext context) {
@@ -24,18 +31,87 @@ class ListImportationState extends State<ListImportation> {
       appBar: new AppBar(
         title: new Text('File Picker Example'),
       ),
-      body: new Center(
-        child: _filePath == null
-            ? new Text('No file selected.')
-            : new Text('Path' + _filePath),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: (){
-
-        },
-        tooltip: 'Select file',
-        child: new Icon(Icons.sd_storage),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            FlatButton(
+              child: Text('Get documents'),
+              onPressed: () {
+                getCSVAndPushDataToFirestore();
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          ContactFromList(ContactListImportation.listDoc)),
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
+
+  //Method to get csv document of the user smartphone and push it to firestore according to field (firstname, lastname, institution)
+  Future getCSVAndPushDataToFirestore() async {
+    File fileToImport;
+
+    //Set the document picker
+    FlutterDocumentPickerParams params = FlutterDocumentPickerParams(
+      allowedFileExtensions: ['csv'],
+      allowedUtiTypes: ['csv'],
+    );
+
+    //Open the document picker (to pick in the smartphone files of the user)
+    final path = await FlutterDocumentPicker.openDocument(params: params);
+
+    //Save the file to import
+    setState(() {
+      fileToImport = File(path);
+    });
+
+    //Separate csv data
+    //Split "\n"
+    final csvContent = fileToImport.readAsStringSync();
+    final splitEachRows = csvContent.split('\n');
+    //Map the data in string
+    final Map<int, String> rowsValue = {
+      for (int i = 0; i < splitEachRows.length; i++) i: splitEachRows[i]
+    };
+
+    //Loop to get each rows
+    for (int i = 0; i < rowsValue.length; i++) {
+      //Split ","
+      final splitEachColumns = rowsValue[i].split(',');
+      //Map the data of each rows
+      final Map<int, String> columnsValue = {
+        for (int i = 0; i < splitEachColumns.length; i++) i: splitEachColumns[i]
+      };
+      //If data is not null push it to firestore (firstname, lastname, institution)
+      if(columnsValue[1] != null){
+        // imageFile = new File('https://eu.ui-avatars.com/api/?name='+columnsValue[0]+"+"+columnsValue[1]);
+        _contactService.addContact(ContactListImportation.listDoc, columnsValue[0], columnsValue[1], columnsValue[2]);
+        // var temp = _contactService.addContact(ContactListImportation.listDoc, columnsValue[0], columnsValue[1], columnsValue[2]);
+        // temp.then((value) {
+        //   uploadImage(firebaseAuth.currentUser.email, value.id);
+        //   up.whenComplete(() => _contactService.addImageLink(value.id));
+        // });
+        // _contactService.addContact(ContactListImportation.listDoc, columnsValue[0], columnsValue[1], columnsValue[2]);
+      }
+    }
+  }
+
+  //Upload image
+  Future uploadImage(String email, String docId) async {
+    ref = FirebaseStorage.instance.ref().child("contacts/$email/$docId");
+    print("image path " + imageFile.path);
+    up = ref.putFile(imageFile);
+  }
+
+  File imageFile;
+  Reference ref;
+  UploadTask up ;
+  FirebaseAuth firebaseAuth = FirebaseAuth.instance;
 }
