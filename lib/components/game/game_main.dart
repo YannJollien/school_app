6e56ca389ab319429fd83bcae96e95b85e048645
aurelienmay <48348880+aurelienmay@users.noreply.github.com>
2021-01-14 19:07:54 +0100

    Navigation modifications

--- lib/components/game/game_main.dart
+++ lib/components/game/game_main.dart
@@ -56,6 +56,22 @@ class _GameScreenState extends State<GameScreen> {
       backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text("Game"),
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      ContactFromList(ContactFromList.listDoc)),
+            );
+          },
+          child: IconTheme(
+            data: Theme.of(context).iconTheme,
+            child: Icon(
+              Icons.arrow_back,
+            ),
+          ),
+        ),
       ),
       body: Column(
         // crossAxisAlignment: CrossAxisAlignment.center,
